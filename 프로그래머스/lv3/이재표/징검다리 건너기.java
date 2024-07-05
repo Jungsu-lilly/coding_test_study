41a6d014cb33781868908e2bc436154eaba67351
jaepyo-Lee <jaepyo99@naver.com>
2024-07-05 12:22:19 +0900

    Update 징검다리 건너기.java

--- 프로그래머스/lv3/이재표/징검다리 건너기.java
+++ 프로그래머스/lv3/이재표/징검다리 건너기.java
@@ -1,52 +1,26 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] stones, int k) {
-        int answer = 0;
-        // 1 3 4 2 1 0 3 1 4 0 -> 1
-        // 0 2 3 1 0 -1 2 0 3 -1 -> 1
-        // -1 1 2 0 -1 -2 1 -1 2 -2 -> 1
-        // -2 0 1 -1 -2 -3 0 -2 1 -3 -> 1
-        while(true){
-            int min=Integer.MAX_VALUE;
-            for(int i=0;i<stones.length;i++){
-                if(min>stones[i] && stones[i]>0){
-                    min=stones[i];
-                }
+        int answer = Integer.MAX_VALUE;
+        Deque<Integer> deque = new LinkedList<>();
+        
+        for (int i = 0; i < stones.length; i++) {
+            while (!deque.isEmpty() && stones[deque.peekLast()] <= stones[i]) {
+                deque.pollLast();
             }
-            int cnt=0;
-            int chk=0;
-            for(int i=0;i<stones.length;i++){
-                stones[i]-=min;
-                if(stones[i]<=0){
-                    cnt++;
-                }else{
-                    cnt=0;
-                }
-                if(cnt>=k){
-                    chk=1;
-                }
+            
+            deque.addLast(i);
+            
+            if (deque.peekFirst() <= i - k) {
+                deque.pollFirst();
             }
-            answer+=min;
-            if(chk!=0){
-                break;
+            
+            if (i >= k - 1) {
+                answer = Math.min(answer, stones[deque.peekFirst()]);
             }
         }
         
         return answer;
-    }
-    public boolean isZeroUnderK(int[]stones,int k){
-        int cnt=0;
-        for(int i=0;i<stones.length;i++){
-            if(stones[i]<=0){
-                cnt++;
-            }else{
-                cnt=0;                
-            }
-            if(cnt>=k){
-                return false;
-            }
-            
-        }
-        return true;
     }
 }
