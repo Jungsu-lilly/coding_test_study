1079f9e2b4902d94987d15b7fccbfcd8967f5ac9
gurwoghd <gurwoghd@gmail.com>
2024-07-15 10:15:54 +0900

    [홍혁재] Day 20

---
+++ 프로그래머스/lv2/홍혁재/타겟 넘버.cpp
@@ -0,0 +1,28 @@
+/*
+알고리즘: 브루트포스
+시간복잡도: O(2^n)
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int bf(vector<int> numbers, int target, int x, int total) {
+    if(x >= numbers.size()) {
+        if(total == target) return 1;
+        else return 0;
+    }
+    for(int i = x; i < numbers.size(); i++) {
+        return bf(numbers, target, i + 1, total + numbers[i]) + 
+            bf(numbers, target, i + 1, total - numbers[i]);;
+    }
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    
+    answer = bf(numbers, target, 0, 0);
+    
+    return answer;
+}
