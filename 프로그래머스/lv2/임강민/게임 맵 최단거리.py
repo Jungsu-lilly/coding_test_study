3657ffb8a610b14eadd34223a3def653ddef395d
KangminLim <87487729+KangminLim@users.noreply.github.com>
2024-07-19 09:46:57 +0900

    [임강민] Day24

---
+++ 프로그래머스/lv2/임강민/게임 맵 최단거리.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+
+def solution(maps):
+    N, M = len(maps), len(maps[0])
+
+    def bfs(ci, cj):
+        q = deque()
+        q.append((ci, cj))
+        v = [[0] * M for _ in range(N)]
+        v[ci][cj] = 1
+
+        while q:
+            ci, cj = q.popleft()
+            if (ci, cj) == (N - 1, M - 1):
+                return v[N - 1][M - 1]
+
+            for ni, nj in ((ci - 1, cj), (ci, cj + 1), (ci + 1, cj), (ci, cj - 1)):
+                if 0 <= ni < N and 0 <= nj < M and not v[ni][nj] and maps[ni][nj]:
+                    q.append((ni, nj))
+                    v[ni][nj] = v[ci][cj] + 1
+
+        return -1
+
+    answer = bfs(0, 0)
+
+    return answer
