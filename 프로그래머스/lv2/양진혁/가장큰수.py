3350f0a723944632894b4027f24c2c74f1eeb6e3
oeyh98 <skusus123@gmail.com>
2024-07-24 10:18:45 +0900

    [양진혁] Day27

--- 프로그래머스/lv2/양진혁/가장큰수.py
+++ 프로그래머스/lv2/양진혁/가장큰수.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     numbers = list(map(str, numbers))
     #자리 수를 맞춘 후, 사전 순 비교
+    #O(nlogn) -> 표준 정렬 + reverse
     numbers.sort(key=lambda x: x*3, reverse=True)
-
     return str(int(''.join(numbers)))
