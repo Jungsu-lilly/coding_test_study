567cca70bd05873845ebe5a8bbc2011d59e2a8ec
724thomas <724thomas@gmail.com>
2024-07-26 10:29:34 +0900

    Day 29

--- 프로그래머스/lv2/최원준/호텔 대실.py
+++ 프로그래머스/lv2/최원준/호텔 대실.py
@@ -4,24 +4,25 @@
 1. 아이디어 :
     interval문제. 스택 사용
 2. 시간복잡도 :
-    O( n )
+    O( nlogn )
 3. 자료구조 :
     힙
 '''
 
-
 import heapq
+
+
 def solution(book_time):
     def serialize(t):
         h, m = t.split(":")
-        return int(h)*60 + int(m)
+        return int(h) * 60 + int(m)
 
-    book_time = sorted([[serialize(start), serialize(end)+10] for start, end in book_time])
+    book_time = sorted([[serialize(start), serialize(end) + 10] for start, end in book_time])
     stack = []
     cmax = 0
 
     for start, end in book_time:
-        while stack and stack[0]<=start:
+        while stack and stack[0] <= start:
             heapq.heappop(stack)
         heapq.heappush(stack, end)
         cmax = max(cmax, len(stack))
