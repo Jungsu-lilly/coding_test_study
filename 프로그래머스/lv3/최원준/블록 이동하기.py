f2e36124e0e962c4cdcb5e18fbc2c326d4997ee3
724thomas <724thomas@gmail.com>
2024-07-29 12:46:17 +0900

    Day 30

--- 프로그래머스/lv3/최원준/블록 이동하기.py
+++ 프로그래머스/lv3/최원준/블록 이동하기.py
@@ -9,49 +9,50 @@
 
 '''
 
-
 from collections import deque
+
+
 def solution(board):
-    n = len(board)-1
+    n = len(board)
+    dir = [[0, 1], [0, -1, ], [1, 0], [-1, 0]]
 
-    def moveable_check(x1, y1):
-        return 0<=x1<n and 0<=y1<n and board[x1][y1] == 0
+    def moveable_check(x, y):
+        return 0 <= x < n and 0 <= y < n and board[x][y] == 0
 
-    def rotate_check(row, col):
-        if moveable_check(row+1, col) and moveable_check(row+1, col+1):
-            return True
-        if moveable_check(row-1, col-1) and moveable_check(row-1, col) and moveable_check(row-1, col+1) and moveable_check(row, col-1) and moveable_check(row+1, col-1) and moveable_check(row+1, col):
-            return True
+    def get_next_positions(x1, y1, x2, y2):
+        next_positions = []
 
-    dir = [[0,1],[1,0]]
+        for dx, dy in dir:  # 상하좌우 이동
+            nx1, ny1, nx2, ny2 = x1 + dx, y1 + dy, x2 + dx, y2 + dy
+            if moveable_check(nx1, ny1) and moveable_check(nx2, ny2):
+                next_positions.append((nx1, ny1, nx2, ny2))
 
-    dp = [[[False, False] for _ in range(len(board)-1)] for _ in range(len(board)-1)] #hor, ver
-    dp[0][0][0] = True
+        if x1 == x2:  # 가로 회전
+            for d in [-1, 1]:
+                if moveable_check(x1 + d, y1) and moveable_check(x2 + d, y2):
+                    next_positions.append((x1, y1, x1 + d, y1))
+                    next_positions.append((x2, y2, x2 + d, y2))
+
+        if y1 == y2:  # 세로 회전
+            for d in [-1, 1]:
+                if moveable_check(x1, y1 + d) and moveable_check(x2, y2 + d):
+                    next_positions.append((x1, y1, x1, y1 + d))
+                    next_positions.append((x2, y2, x2, y2 + d))
+        return next_positions
 
     queue = deque()
-    queue.append((0,0,0))
+    queue.append(((0, 0, 0, 1), 0))
+    visited = set()
+    visited.add((0, 0, 0, 1))
 
     while queue:
-        row1, col1, is_ver = queue.popleft()
-        print(row1, col1, is_ver)
+        (x1, y1, x2, y2), time = queue.popleft()
 
-        if not is_ver and rotate_check(row1, col1):
-            dp[row1][col1][1] = True
-            queue.append((row1, col1, 1))
-        for row2, col2 in dir:
-            nrow, ncol = row1+row2, col1+col2
-            if not moveable_check(nrow, ncol):
+        if (x1, y1) == (n - 1, n - 1) or (x2, y2) == (n - 1, n - 1):
+            return time
+
+        for next_position in get_next_positions(x1, y1, x2, y2):
+            if next_position in visited:
                 continue
-            if is_ver and not moveable_check(nrow, ncol+1):
-                continue
-            elif not is_ver and not moveable_check(nrow+1, ncol):
-                continue
-            dp[nrow][ncol][is_ver] = True
-            queue.append((nrow, ncol, is_ver))
-
-    print(*dp, sep='\n')
-    return
-
-
-
-
+            visited.add(next_position)
+            queue.append((next_position, time + 1))
