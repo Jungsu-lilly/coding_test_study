b1f7082ca8fa5043e114520b4eadb53e509c72d7
seoyeong200 <youngseogimkk@gmail.com>
2024-07-31 10:23:31 +0900

    Day32

--- 프로그래머스/lv2/김서영/148653.py
+++ 프로그래머스/lv2/김서영/148653.py
@@ -21,24 +21,27 @@ def solution(storey):
             (554,2)             (546,3)
         (54,2+5)  (46,2+6)      (46,3+5)  (54,3+6)
         """
-        nonlocal answer
-        # if (num, stone) in visited: return
-        if len(num) == 1:
-            answer = min(answer, stone+int(num))
-            return answer
-        # visited.add((num, stone))
+        if num in visited: return visited[num]
+        # 한자리수인경우 0-5는 num, 6-9는 각각 5,4,3,2 리턴해야함
+        if len(num) == 1: 
+            if num in [6,7,8,9]: return 11-int(num)+stone
+            return int(num)+stone
+
         multiply = 10**(len(num)-1)
         lower = int(num[0])
         upper = int(num[0])+1
-        
-        return min(
+
+        ans = min(
             backtrack(str(int(num) - lower*multiply), stone+lower),
             backtrack(str(upper*multiply - int(num)), stone+upper)
         )
 
-    answer = 1e10
-    backtrack(str(storey), 0)
+        visited[num] = ans
+        return ans
     
+    visited = {}
+    answer = backtrack(str(storey), 0)
+    print(answer)
     return answer
 
 solution(16)
