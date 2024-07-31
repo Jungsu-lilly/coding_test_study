1c57dfd0d2fdf1f99c69914453063ac317c6a148
724thomas <724thomas@gmail.com>
2024-07-31 10:11:25 +0900

    Day 32

--- 프로그래머스/lv3/최원준/코딩 테스트 공부.py
+++ 프로그래머스/lv3/최원준/코딩 테스트 공부.py
@@ -9,7 +9,6 @@
     배열
 '''
 
-
 def solution(alp, cop, problems):
     max_alp = max([problem[0] for problem in problems])
     max_cop = max([problem[1] for problem in problems])
@@ -20,17 +19,17 @@ def solution(alp, cop, problems):
 
     for alp_score in range(alp, max_alp + 1):
         for cop_score in range(cop, max_cop + 1):
+            curr_time = dp[alp_score][cop_score]
             if alp_score + 1 <= max_alp:  # +1 alp
-                dp[alp_score + 1][cop_score] = min(dp[alp_score + 1][cop_score], dp[alp_score][cop_score] + 1)
+                dp[alp_score + 1][cop_score] = min(dp[alp_score + 1][cop_score], curr_time + 1)
             if cop_score + 1 <= max_cop:  # +1 cop
-                dp[alp_score][cop_score + 1] = min(dp[alp_score][cop_score + 1], dp[alp_score][cop_score] + 1)
+                dp[alp_score][cop_score + 1] = min(dp[alp_score][cop_score + 1], curr_time + 1)
 
             for problem in problems:
                 nalp, ncop, palp, pcop, time = problem
-
                 if alp_score >= nalp and cop_score >= ncop:
                     ni = min(max_alp, alp_score + palp)
                     nj = min(max_cop, cop_score + pcop)
-                    dp[ni][nj] = min(dp[ni][nj], dp[alp_score][cop_score] + time)
+                    dp[ni][nj] = min(dp[ni][nj], curr_time + time)
 
     return dp[max_alp][max_cop]
