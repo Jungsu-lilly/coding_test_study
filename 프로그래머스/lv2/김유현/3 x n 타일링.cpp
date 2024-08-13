2be6790d59726c52e8fe6351f1071320bf91e28a
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-13 13:28:08 +0900

    [김유현] Day 41

--- 프로그래머스/lv2/김유현/3 x n 타일링.cpp
+++ 프로그래머스/lv2/김유현/3 x n 타일링.cpp
@@ -8,17 +8,18 @@ long long dp[5004];
 
 int solution(int n) {    
     if (n%2) {
-      return 0;  
+        return 0;  
     } 
     
     dp[0] = 1;
     dp[2] = 3; 
     
-    for(int i=4; i<=n; i++) {
+    for(int i=4; i<=n; i+=2) {
         dp[i] = dp[i-2] * 3;
-        for(int j=4; j<=i; j++) {
+        for(int j=4; j<=i; j+=2) {
             dp[i] += dp[i-j] * 2;
         }
+        dp[i] %= MOD;
     }
     
     return dp[n] % MOD; 
