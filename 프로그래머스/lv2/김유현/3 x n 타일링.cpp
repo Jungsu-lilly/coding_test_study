1e3cadc37925f2d16cf370a751e6060b1dc7bd6d
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-13 10:34:12 +0900

    (WIP) [김유현] Day 41

---
+++ 프로그래머스/lv2/김유현/3 x n 타일링.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+#define MOD 1000000007
+using namespace std;
+
+long long dp[5004];
+
+int solution(int n) {    
+    if (n%2) {
+      return 0;  
+    } 
+    
+    dp[0] = 1;
+    dp[2] = 3; 
+    
+    for(int i=4; i<=n; i++) {
+        dp[i] = dp[i-2] * 3;
+        for(int j=4; j<=i; j++) {
+            dp[i] += dp[i-j] * 2;
+        }
+    }
+    
+    return dp[n] % MOD; 
+}
