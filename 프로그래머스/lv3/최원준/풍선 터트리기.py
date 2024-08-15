78f0fbab1be42f2d8d285670ed7c40cab8b136e9
724thomas <724thomas@gmail.com>
2024-08-15 10:28:30 +0900

    Day 43

--- 프로그래머스/lv3/최원준/풍선 터트리기.py
+++ 프로그래머스/lv3/최원준/풍선 터트리기.py
@@ -9,17 +9,11 @@
 
 '''
 
-
 def solution(a):
     n = len(a)
-    if n == 1:
-        return 1
 
-    left_min = [0] * n
-    left_min[0] = a[0]
-
-    right_min = [0] * n
-    right_min[n - 1] = a[n - 1]
+    left_min = [a[0]] * n
+    right_min = [a[n-1]] * n
 
     # 왼쪽 최소값 계산
     for i in range(1, n):
@@ -29,15 +23,7 @@ def solution(a):
     for i in range(n - 2, -1, -1):
         right_min[i] = min(right_min[i + 1], a[i])
 
-    print(left_min)
-    print(right_min)
-
-    answer = 0
-    for i in range(n):
-        if a[i] <= left_min[i] or a[i] <= right_min[i]:
-            answer += 1
-
-    return answer
+    return len(a) - sum([1 for i in range(n) if right_min[i] < a[i] > left_min[i]])
 
 
 
